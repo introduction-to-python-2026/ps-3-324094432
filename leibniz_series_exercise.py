c6eaf24523ae4c43d3428d22a6bd6fe1b70fcb65
324094432 <guy.kaizer@gmail.com>
2025-11-07 14:22:13 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,7 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    list_of_alternating_signs = []
+        for i in range(n_terms):
+            list_of_alternating_signs.append((-1)**i/(2*i+1))
+        
+        pi = 4*sum(list_of_alternating_signs)
+        return pi
